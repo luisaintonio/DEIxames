4f4e9fbdd7b0cfcd85cc39e276e50fc1048f391a
Fábio Ferreira <Fbio Ferreira>
2016-11-29 01:45:11 +0000

    loadPessoas done!!!

--- src/deixames/DEIxames.java
+++ src/deixames/DEIxames.java
@@ -1,10 +1,14 @@
 
 package deixames;
 
+import java.io.IOException;
+
 public class DEIxames {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // onde vai ficar o menu ou eventualmente a interface grafica
+        GestorDEIxames g=new GestorDEIxames();
+        g.loadPessoas("pessoas.txt");
     }
     
 }
